4cf5dad7eaf007389359f9b1616789d948390c51
uchiyin <89871444+uchiyin@users.noreply.github.com>
2023-02-06 23:45:55 -0800

    + position functions and parent constraint

--- locatorAtJoints.py
+++ locatorAtJoints.py
@@ -1,4 +1,5 @@
-# Creates a locator for each selected joint at the joint's world position. Locator named after joint.
+# Creates a locator for each selected joint at the joint's world position.
+# Freezes locator transforms and centers pivot; parent constrains each joint to locator.
 
 from maya import cmds
 
@@ -12,7 +13,13 @@ if jnts:
         objNameSplit = obj.split('_')
         nameLen = len(objNameSplit);
         objName = '_'.join(objNameSplit[:nameLen-1])
-        cmds.spaceLocator(p=jntPos, n=objName + '_loc')
+        loc = cmds.spaceLocator(p=jntPos, n=objName + '_loc')
+        cmds.makeIdentity(loc, apply=True, t=1, r=1, s=1, n=0)
+        cmds.xform(loc, centerPivots = True)
+        
+        ### constrain joints to locators
+        cmds.parentConstraint(loc, obj, mo = True);
+        
 else:
     print('no joints selected');
 
