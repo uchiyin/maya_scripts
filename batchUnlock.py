859f7b2d2f2835023fcf8d625dec7a14f6f9bbc9
uchiyin <89871444+uchiyin@users.noreply.github.com>
2023-02-18 17:41:53 -0800

    Update batchUnlock.py

--- batchUnlock.py
+++ batchUnlock.py
@@ -1,3 +1,4 @@
+# functions as the opposite "lock and hide all" function in the channel box, instead unlocking and showing all channels. Works for multiple objects.
 import maya.cmds as cmds
 
 objs = cmds.ls(sl=True)
